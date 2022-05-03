7ea5c3df2d3eaa317800ab973489524993d52051
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-05-03 17:22:46 +0530

    Django project

---
+++ home/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from home import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('contact', views.contact, name='contact'),
+    path('login', views.loginUser, name='loginUser'),
+    path('logout', views.logoutUser, name='logoutUser')
+]
