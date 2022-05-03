21a2687a38975b97c4c9a8751a845a4bf055c4c9
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-05-03 17:31:50 +0530

    Django project

--- home/views.py
+++ home/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, logout, login
+
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+def loginUser(request):
+    return render(request, 'login.html')
+
+
+def logoutUser(request):
+    return redirect('/login')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
