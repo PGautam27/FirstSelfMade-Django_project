479750f28dbccad419da0019726d2d60c666cc37
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-05-05 15:55:49 +0530

    Django project

--- home/views.py
+++ home/views.py
@@ -9,18 +9,18 @@ from home.models import Contact
 def index(request):
     if request.user.is_anonymous:
         return redirect("/login")
-    return render(request, 'index.html')
+    return render(request, "index.html")
 
 
 def loginUser(request):
     if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        user = authenticate(username, password)
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
             return redirect("/")
-    return render(request, 'login.html')
+    return render(request, "login.html")
 
 
 def logoutUser(request):
