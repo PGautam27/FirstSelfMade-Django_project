addec6f0ae59800d5a306a66a3d4cc5be4894ac9
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-05-05 15:45:25 +0530

    Django project

--- home/views.py
+++ home/views.py
@@ -7,14 +7,24 @@ from home.models import Contact
 
 # Create your views here.
 def index(request):
+    if request.user.is_anonymous:
+        return redirect("/login")
     return render(request, 'index.html')
 
 
 def loginUser(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(username, password)
+        if user is not None:
+            login(request, user)
+            return redirect("/")
     return render(request, 'login.html')
 
 
 def logoutUser(request):
+    logout(request)
     return redirect('/login')
 
 
