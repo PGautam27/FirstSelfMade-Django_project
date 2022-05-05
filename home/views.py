35661c106c6c7fe97ec4dd3ce5996c1842d2862d
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-05-05 14:26:13 +0530

    Django project

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, logout, login
+from home.models import Contact
 
 
 # Create your views here.
@@ -16,5 +17,11 @@ def logoutUser(request):
 
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contacts = Contact(name, email, phone, desc)
+        contacts.save()
     return render(request, 'contact.html')
-
