ea416481e2c72ac6ab0e32454dd08495bf086a07
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-05-05 14:54:48 +0530

    Django project

--- home/models.py
+++ home/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Contact(models.Model):
     name = models.CharField(max_length=122, null=True)
-    email = models.EmailField(max_length=122)
-    phone = models.CharField(max_length=10)
-    text = models.TextField()
+    email = models.CharField(max_length=122, null=True)
+    phone = models.CharField(max_length=10, null=True)
+    desc = models.TextField(null=True)
     date = models.DateField()
