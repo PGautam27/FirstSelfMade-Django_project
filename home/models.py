c40b9a7b6139001c8d88d0399ec688f7aa9d20ce
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-05-05 15:19:27 +0530

    Django project

--- home/models.py
+++ home/models.py
@@ -8,3 +8,8 @@ class Contact(models.Model):
     phone = models.CharField(max_length=10, null=True)
     desc = models.TextField(null=True)
     date = models.DateField()
+
+
+class Photo(models.Model):
+    mine = models.CharField(max_length=12)
+    date = models.DateField()
